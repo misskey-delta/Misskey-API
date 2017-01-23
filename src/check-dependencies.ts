8b426a641f7ce534dd1a4b4c5a2f4a67b238f7fa
petitsurume <gamemannew2013@yahoo.co.jp>
2017-01-23 22:54:25 +0900

    そういやここいらんな

--- src/check-dependencies.ts
+++ src/check-dependencies.ts
@@ -2,15 +2,11 @@ import {logInfo, logDone, logWarn} from 'log-cool';
 import {exec} from 'child_process';
 
 export default function(): void {
-	try {
-		checkDependency('Node.js', 'node -v', x => x.match(/^v(.*)\r?\n$/)[1]);
-		checkDependency('npm', 'npm -v', x => x.match(/^(.*)\r?\n$/)[1]);
-		checkDependency('MongoDB', 'mongo --version', x => x.match(/^MongoDB shell version: (.*)\r?\n$/)[1]);
-		checkDependency('Redis', 'redis-server --version', x => x.match(/v=([0-9\.]*)/)[1]);
-		logDone('Checked external dependencies');
-	} catch(e) {
-		logWarn('Check dependencies error')
-	}
+	checkDependency('Node.js', 'node -v', x => x.match(/^v(.*)\r?\n$/)[1]);
+	checkDependency('npm', 'npm -v', x => x.match(/^(.*)\r?\n$/)[1]);
+	checkDependency('MongoDB', 'mongo --version', x => x.match(/^MongoDB shell version: (.*)\r?\n$/)[1]);
+	checkDependency('Redis', 'redis-server --version', x => x.match(/v=([0-9\.]*)/)[1]);
+	logDone('Checked external dependencies');
 }
 
 function checkDependency(serviceName: string, command: string, transform: (x: string) => string): void {
