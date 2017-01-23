5c22ba102f90d6fd8e05b4ae111d2474906a155f
petitsurume <gamemannew2013@yahoo.co.jp>
2017-01-23 23:10:56 +0900

    ここいらんって言われた

--- src/check-dependencies.ts
+++ src/check-dependencies.ts
@@ -10,21 +10,16 @@ export default function(): void {
 }
 
 function checkDependency(serviceName: string, command: string, transform: (x: string) => string): void {
-	try {
-		exec(command, (error, stdout, stderr) => {
-			try {
-				if (error) {
-					logWarn(`Unable to find ${serviceName}`);
-				} else {
-					logInfo(`${serviceName} ${transform(stdout.toString())}`);
-				}
-			} catch (e) {
-				console.error(e);
-				logWarn(`Check dependencies error (${serviceName})`);
+	exec(command, (error, stdout, stderr) => {
+		try {
+			if (error) {
+				logWarn(`Unable to find ${serviceName}`);
+			} else {
+				logInfo(`${serviceName} ${transform(stdout.toString())}`);
 			}
-		});
-	} catch (e) {
-		console.error(e);
-		logWarn(`Check dependencies error (${serviceName})`);
-	}
+		} catch (e) {
+			console.error(e);
+			logWarn(`Check dependencies error (${serviceName})`);
+		}
+	});
 }
