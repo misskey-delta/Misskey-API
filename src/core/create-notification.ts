8a799b1e325613159f2b9573eca6f831f9ed6cfb
otofune <otofune@gmail.com>
2017-04-24 12:21:20 +0900

    [core/create-notification] stop sending same notification in 10 min
    resolve #32/

--- src/core/create-notification.ts
+++ src/core/create-notification.ts
@@ -2,25 +2,46 @@ import {Notification} from '../db/db';
 import {IApplication, INotification} from '../db/interfaces';
 import event from '../event';
 
-export default function(
+export default (
 	app: IApplication,
 	userId: string,
 	type: string,
 	content: any
-): Promise<INotification> {
-	return new Promise<INotification>((resolve, reject) => {
+): Promise<INotification> => new Promise ((resolve, reject) => {
+	// get lastOne
+	Notification.find({
+		app: app || null,
+		user: userId,
+		type: type
+	}).limit(1).sort('-$natural').exec((err: any, notifications: INotification[]) => {
+		const lastOneNotification: INotification = notifications[0] || null;
+		// check notify already sent.
+		if (lastOneNotification) {
+			// check lastOne's content whether it's same as passed content
+			const similar = Object.keys(lastOneNotification.content).every(
+				key => lastOneNotification.content[key] === content[key]
+			)
+			// if similar, calc delay.
+			if (similar) {
+				const createdAt: Date = lastOneNotification.createdAt;
+				const delay = Date.now() - createdAt.getTime();
+				// notified similer notification within 10 min, stop.
+				if (delay < (10 * 60 * 1000)) {
+					resolve(lastOneNotification);
+					return;
+				}
+			}
+		}
+		// create
 		Notification.create({
-			app: app !== null ? app.id : null,
+			app: app || null,
 			user: userId,
 			type: type,
 			content: content
-		}, (createErr: any, createdNotification: INotification) => {
-			if (createErr !== null) {
-				reject(createErr);
-			} else {
-				resolve(createdNotification);
-				event.publishNotification(createdNotification);
-			}
+		}, (err:any, createdNotification: INotification) => {
+			if (err) reject(err);
+			event.publishNotification(createdNotification);
+			resolve(createdNotification);
 		});
 	});
-}
+});
