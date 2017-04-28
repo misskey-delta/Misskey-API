407c8de58c4b4a72ae6bc3064cf482e871e9e193
otofune <otofune@gmail.com>
2017-04-28 17:00:12 +0900

    [db] follow lint

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,13 +2,13 @@ import * as mongoose from 'mongoose';
 import config from '../config';
 
 // use native promise
-(<any>mongoose).Promise = global.Promise
+(<any>mongoose).Promise = global.Promise;
 const options = Object.assign(
-    {
-        promiseLibrary: global.Promise
-    },
-    config.mongo.options
-)
+	{
+		promiseLibrary: global.Promise
+	},
+	config.mongo.options
+);
 
 const db = mongoose.createConnection(config.mongo.uri, options);
 
